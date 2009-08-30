70f1ea9081ddf6622e6ea5f0a6da88ab81f399fb
Cory Watson <github@onemogin.com>
2009-08-30 09:53:52 -0500

    Add FormValidator consumer.

---
+++ lib/Message/Stack/FormValidator.pm
@@ -0,0 +1,32 @@
+package Message::Stack::FormValidator;
+use Moose;
+
+use Message::Stack::Message;
+
+sub parse {
+    my ($self, $stack, $scope, $results) = @_;
+
+    if($results->success) {
+        return 1;
+    }
+
+    foreach my $f ($results->missing) {
+        $stack->add(Message::Stack::Message->new(
+            id      => "missing_$f",
+            scope   => $scope,
+            subject => $f
+        ));
+    }
+
+    foreach my $f ($results->invalid) {
+        $stack->add(Message::Stack::Message->new(
+            id      => "invalid_$f",
+            scope   => $scope,
+            subject => $f
+        ));
+    }
+
+    return 0;
+}
+
+1;
